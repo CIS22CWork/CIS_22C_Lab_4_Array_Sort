dca66682153f08f399bb26d6faec5bbbc46414eb
SeeLtephen <32529372+SeeLtephen@users.noreply.github.com>
2017-11-13 14:10:04 -0800

    Some string changes

--- ArraySort_BL_SL_AM/FileIO.cpp
+++ ArraySort_BL_SL_AM/FileIO.cpp
@@ -5,6 +5,10 @@ bool FileIO::strToFile (std::string str, std::string filePath)
 	bool flag = false;
 	std::ofstream myFile;
 	myFile.open (filePath);
+	myFile << "Branden Lee, Stephen Lee, and Alex Morfin" << std::endl;
+	myFile << "CIS 22C" << std::endl;
+	myFile << "Fall 2017" << std::endl;
+	myFile << "Lab 4" << std::endl;
 	if (myFile)
 	{
 		myFile << str;
